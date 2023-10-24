0bec3108b783631bfc48cbbee19a5f13d4d5ac76
trem 7 <oxetaligado@gmail.com>
2023-10-24 14:10:00 -0300

    trem 7
    dddd

---
+++ if.py
@@ -0,0 +1,8 @@
+print('ola')
+nome = input('Digite seu nome')
+idade= int(input('Digite sua idade'))
+time= input('Digite seu time')
+if time=='Corinthians':
+    print('certo')
+else:
+    print('time ruim da peste')
