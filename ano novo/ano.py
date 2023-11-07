a586ae719155fa065ce034ac7f800949b4231dba
jpedograu <ultrasenha@gmail.com>
2023-11-07 11:06:08 -0300

    terminei

---
+++ ano novo/ano.py
@@ -0,0 +1,5 @@
+from time import sleep
+for ano in range(10,-1,-1):
+    print(ano)
+    sleep(1.0)
+print('LERE UIPIII HA SHIIIIIP')
--- if.py
+++
@@ -1,8 +0,0 @@
-print('ola')
-nome = input('Digite seu nome')
-idade= int(input('Digite sua idade'))
-time= input('Digite seu time')
-if time=='Corinthians':
-    print('certo')
-else:
-    print('time ruim da peste')
