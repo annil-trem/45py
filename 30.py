dd989331906448823eb38cbf6d37e7eacf2c9852
annil-trem <144158461+annil-trem@users.noreply.github.com>
2023-11-20 16:37:35 -0300

    Update 30.py

--- 30.py
+++ 30.py
@@ -1,28 +1,27 @@
-valor_da_hora = float(input("Digite quanto você recebe por hora: "))
-horas_trabalhadas = float(
-    input("Digite quantas horas você trabalhou esse mês: ")
-)
-salario_bruto = valor_da_hora * horas_trabalhadas
-if salario_bruto <= 900:
+v_d_h = float(input("Digite quanto você recebe por hora: "))
+h_t = float(
+    input("Digite quantas horas você trabalhou esse mês: "))
+s_b = v_d_h * horas_trabalhadas
+if s_b<= 900:
     desconto_ir = 0.0
-elif salario_bruto <= 1500:
+elif s_b <= 1500:
     desconto_ir = 5
-elif salario_bruto <= 2500:
+elif s_b <= 2500:
     desconto_ir = 10
 else:
     desconto_ir = 20
 
-IR = salario_bruto * (desconto_ir / 100)
-INSS = salario_bruto * (10 / 100)
-FGTS = salario_bruto * (11 / 100)
-total_de_descontos = IR + INSS
+IR = s_b * (desconto_ir / 100)
+INSS = s_b* (10 / 100)
+FGTS = s_b * (11 / 100)
+t_d_des = IR + INSS
 salario_liquido = salario_bruto - total_de_descontos
 
 print(
-    f"\nSalário Bruto     : R${salario_bruto:.2f}",
+    f"\nSalário Bruto     : R${s_b:.2f}",
     f"\n(-) IR (5%)       : R${IR:.2f}",
     f"\n(-) INSS ( 10%)   : R${INSS:.2f}",
     f"\nFGTS (11%)        : R${FGTS:.2f}",
-    f"\nTotal de descontos: R${total_de_descontos:.2f}",
-    f"\nSalário Liquido   : R${salario_liquido:.2f}",
+    f"\nTotal de descontos: R${t_d_des:.2f}",
+    f"\nSalário Liquido   : R${s_l:.2f}",
 )
