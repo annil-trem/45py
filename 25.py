3dcbfb22bbef0e2a5aa0d26222df284f86132ad2
annil-trem <144158461+annil-trem@users.noreply.github.com>
2023-11-20 16:32:48 -0300

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,16 +1,16 @@
-numero1 = float(input("Digite um numero: "))
-numero2 = float(input("Digite outro numero: "))
-numero3 = float(input("Digite mais um numero: "))
-if numero1 > numero2 and numero1 > numero3:
-    print(f"{numero1} foi o maior numero digitado.")
-elif numero2 > numero1 and numero2 > numero3:
-    print(f"{numero2} foi o maior numero digitado.")
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+n3 = float(input("Digite mais um numero: "))
+if n1 > n2 and n1 > n3:
+    print(f"{n1} foi o maior numero digitado.")
+elif n2 > n1 and n2 > n3:
+    print(f"{2} foi o maior numero digitado.")
 else:
-    print(f"{numero3} foi o maior numero digitado.")
+    print(f"{n3} foi o maior numero digitado.")
 
-if numero1 < numero2 and numero1 < numero3:
-    print(f"{numero1} foi o menor numero digitado.")
+if n1 < n2 and n1 < n3:
+    print(f"{n1} foi o menor numero digitado.")
 elif numero2 < numero1 and numero2 < numero3:
-    print(f"{numero2} foi o menor numero digitado.")
+    print(f"{n2} foi o menor numero digitado.")
 else:
-    print(f"{numero3} foi o menor numero digitado.")
+    print(f"{n3} foi o menor numero digitado.")
