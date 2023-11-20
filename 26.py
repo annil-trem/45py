a2d1e3259525ec0634589d04b1d303902f91f499
annil-trem <144158461+annil-trem@users.noreply.github.com>
2023-11-20 16:34:24 -0300

    Update 26.py

--- 26.py
+++ 26.py
@@ -1,9 +1,9 @@
-preco1 = float(input("Digite o preço do produto 1: "))
-preco2 = float(input("Digite o preço do produto 2: "))
-preco3 = float(input("Digite o preço do produto 3: "))
-if preco1 < preco2 and preco1 < preco3:
-    print(f"O produto com o menor preco é o 1, custando R${preco1:.2f}")
-elif preco2 < preco1 and preco2 < preco3:
-    print(f"O produto com o menor preco é o 2, custando R${preco2:.2f}")
+pç1 = float(input("Digite o preço do produto 1: "))
+pç2 = float(input("Digite o preço do produto 2: "))
+pç3 = float(input("Digite o preço do produto 3: "))
+if pç1 < pç2 and pç1 < pç3:
+    print(f"O produto com o menor preco é o 1, custando R${pç1:.2f}")
+elif pç2 < pç1 and pç2 < pç3:
+    print(f"O produto com o menor preco é o 2, custando R${pç2:.2f}")
 else:
-    print(f"O produto com o menor preco é o 3, custando R${preco3:.2f}")
+    print(f"O produto com o menor preco é o 3, custando R${pç3:.2f}")
